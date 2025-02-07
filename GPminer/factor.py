625cb229c31f2fd44de91aa660d805f83e4fcd7d
LinHanLi <h1nlee@163.com>
2025-02-07 20:25:45 +0800

    Update factor.py

--- GPminer/factor.py
+++ GPminer/factor.py
@@ -10,12 +10,12 @@ import FreeBack as FB
 # method：时序算符, period: 时序算符周期
 class Factor():
     def __init__(self, market):
-        for i in ['close', 'open', 'high', 'low', 'amount', 'vol']:
+        self.market = market
+        for i in ['close', 'open', 'high', 'low', 'pre_close', 'amount', 'vol']:
             if i not in market.columns:
                 raise ValueError("value must include %s"%i)
-        if ('pre_close' not in market.columns) and ('exfactor' not in market.columns):
-            raise ValueError('value must include pre_close or exfactor')
-        self.market = market
+        if 'exfactor' not in market.columns:
+            self.cal_factor('exfactor')
     # 计算/引用因子
     def cal_factor(self, code):
         if code in self.market.columns:
@@ -36,38 +36,51 @@ class Factor():
         para = code_split[1:]
         if key=='exfactor':    # 复权因子
             exfactor = self.cal_factor('close').groupby('code').shift()/self.cal_factor('pre_close')
-            self.market[code] = exfactor.groupby('code').cumprod() 
+            self.market[code] = exfactor.groupby('code').cumprod()
+        # 量价基础 basic
         elif key in ['ex_close', 'ex_open', 'ex_high', 'ex_low']:
             self.market[code] = self.cal_factor(code[3:])*self.cal_factor('exfactor')
         elif key=='vwap':     
             self.market[code] = self.cal_factor('amount')/self.cal_factor('vol')
+        # 动量反转  mom
         elif key=='Ret':      
-            self.market[code] = self.cal_factor('ex_close')/self.cal_factor('ex_close').groupby('code').shift()
+            self.market[code] = self.cal_factor('close')/self.cal_factor('pre_close')
+        elif key=='nightRet':     # 隔夜收益（开盘跳空
+            self.market[code] = self.cal_factor('open')/self.cal_factor('pre_close')-1
+        elif key=='T0Ret':
+            self.market['T0Ret'] = self.cal_factor('close')/self.cal_factor('open')-1
+        elif key=='maxRet':
+            self.market['maxRet'] = self.cal_factor('high')/self.cal_factor('low')-1
+        elif key=='bodyRatio':   # k线实体占比
+            self.market[code] = abs(self.cal_factor('close')-self.cal_factor('open'))/(self.cal_factor('high')-self.cal_factor('low'))
         elif key=='RSI':     
-            up = pd.Series(np.where(self.cal_factor('ret')>0, self.cal_factor('ret'), 0), \
+            up = pd.Series(np.where(self.cal_factor('Ret')>0, self.cal_factor('Ret'), 0), \
                            index=self.market.index)
-            down = pd.Series(np.where(self.cal_factor('ret')<0, -self.cal_factor('ret'), 0), \
+            down = pd.Series(np.where(self.cal_factor('Ret')<0, -self.cal_factor('Ret'), 0), \
                              index=self.market.index)
             sumup = FB.my_pd.cal_ts(up, 'Sum', int(para[0]))
             sumdown = FB.my_pd.cal_ts(down, 'Sum', int(para[0]))
             rsi = sumup/(sumup+sumdown)
             self.market[code] = rsi
-        elif key=='DrawDown':        # 唐奇安通道 上轨距离
+        elif key=='DrawDown':        # 唐奇安通道 上轨距离   DrawDown.d
             max_c = FB.my_pd.cal_ts(self.cal_factor('ex_high'), 'Max', int(para[0]))
             self.market[code] = self.cal_factor('ex_close')/min_c-1 
-        elif key=='DrawUp':
+        elif key=='DrawUp':   # 唐奇安通道 下轨距离  DrawUp.d
             min_c = FB.my_pd.cal_ts(self.cal_factor('ex_low'), 'Min', int(para[0]))
             self.market[code] = 1-self.cal_factor('ex_close')/max_c
-        elif key=='AMP':   # 振幅
+        # 波动率  volatility
+        elif key=='AMP':   # 振幅   AMP.d
             exhigh_Max = FB.my_pd.cal_ts(self.cal_factor('ex_high'), 'Max', int(para[0]))
             exlow_Min = FB.my_pd.cal_ts(self.cal_factor('ex_low'), 'Min', int(para[0]))
-            self.market[code] = (exhigh_Max - exlow_Min)/exlow_Min  
+            self.market[code] = (exhigh_Max - exlow_Min)/exlow_Min
         elif key=='TR':    # 真实波动
-            self.market[code] = pd.concat([self.cal_factor('ex_high')-self.cal_factor('ex_low'),\
-                abs(self.cal_factor('ex_close').groupby('code').shift()-self.cal_factor('ex_high')), \
-                abs(self.cal_factor('ex_close').groupby('code').shift()-self.cal_factor('ex_low'))], axis=1).max(axis=1) 
+            self.market[code] = pd.concat([self.cal_factor('high')-self.cal_factor('low'),\
+                abs(self.cal_factor('pre_close')-self.cal_factor('high')), \
+                abs(self.cal_factor('pre_close')-self.cal_factor('low'))], axis=1).max(axis=1) 
+        # 成交流动性   vol
         elif key=='Amihud':  # 流动性
             self.market[code] = self.cal_factor('Ret')/self.cal_factor('amount')
+        # 技术形态  kbars
         elif key=='MACD':   #  MACD.d.d.d 
             EMAslow = FB.my_pd.cal_ts(self.cal_factor('ex_close'), 'EMA', int(para[1])) 
             EMAfast = FB.my_pd.cal_ts(self.cal_factor('ex_close'), 'EMA', int(para[0]))
@@ -78,10 +91,26 @@ class Factor():
             deltax = self.cal_factor('ex_low')-FB.my_pd.cal_ts(self.cal_factor('ex_low'), 'MA', int(para[0]))
             deltay = self.cal_factor('ex_high')-FB.my_pd.cal_ts(self.cal_factor('ex_high'), 'MA', int(para[0]))
             self.market[code] = FB.my_pd.cal_ts(deltax*deltay, 'Sum', int(para[0]))/FB.my_pd.cal_ts(deltax**2, 'Sum', int(para[0])) 
-        elif key=='VolPriceCorr':  # 量价相关性   VolPriceCorr.d
+        # 量价相关  composite
+        elif key in ['VolVWAPCorr', 'VolAMPCorr', 'VolRetCorr']:  # 量价相关性   VolPriceCorr.d
             deltax = self.cal_factor('vol')-FB.my_pd.cal_ts(self.cal_factor('vol'), 'MA', int(para[0]))
-            deltay = self.cal_factor('ex_close')-FB.my_pd.cal_ts(self.cal_factor('ex_close'), 'MA', int(para[0]))
+            if key=='VolVWAPCorr':            
+                deltay = self.cal_factor('vwap')-FB.my_pd.cal_ts(self.cal_factor('vwap'), 'MA', int(para[0]))
+            elif key=='VolAMPCorr':
+                deltay = self.cal_factor('AMP.1')-FB.my_pd.cal_ts(self.cal_factor('AMP.1'), 'MA', int(para[0]))
+            elif key=='VolRetCorr':
+                deltay = self.cal_factor('Ret')-FB.my_pd.cal_ts(self.cal_factor('Ret'), 'MA', int(para[0]))
             self.market[code] = (FB.my_pd.cal_ts((deltax*deltay), 'Sum', int(para[0]))/\
                     (np.sqrt(FB.my_pd.cal_ts(deltax**2, 'Sum', int(para[0])))*\
                      np.sqrt(FB.my_pd.cal_ts(deltay**2, 'Sum', int(para[0]))))).fillna(0)
+        elif key=='UnusualVol':    # 异常成交量  UnusualVol.d
+            self.market[code] = self.cal_factor('vol')/self.cal_factor('vol_MA_'+para[0])
+        # 金融学理论   model        
+        elif key=='beta':    # CAMP理论 beta/alpha/estd  beta.d
+            # 计算市场平均收益
+            deltax = self.cal_factor('meanRet')-FB.my_pd.cal_ts(self.cal_factor('meanRet'), 'MA', int(para[0]))
+            deltay = self.cal_factor('Ret')-FB.my_pd.cal_ts(self.cal_factor('Ret'), 'MA', int(para[0]))
+            self.market[code] = FB.my_pd.cal_ts(deltax*deltay, 'Sum', int(para[0]))/FB.my_pd.cal_ts(deltax**2, 'Sum', int(para[0])) 
+        elif key=='oc2hl':   # 开收重心到低高重心变动
+            self.market[code] = (self.cal_factor('high')+self.cal_factor('low'))/(self.cal_factor('open')+self.cal_factor('close')) 
         return self.market[code]
